f31d237f67e02893b4e3a3fb42ac69d8e25fb076
star-sil <kimse9811@naver.com>
2023-09-27 22:59:26 +0900

    init

---
+++ src/main/java/def/agoramain/AgoraMainApplication.java
@@ -0,0 +1,13 @@
+package def.agoramain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoraMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgoraMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/def/agoramain/AgoraMainApplicationTests.java
@@ -0,0 +1,13 @@
+package def.agoramain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
