859a014442394fb0b67f653101395b9a256b894d
BaeKwangho <qorhkdgh00@gmail.com>
2023-12-01 01:03:30 +0900

    feat: 유저 검증 추가

--- src/main/java/def/agoramain/review/problem/controller/ProblemController.java
+++ src/main/java/def/agoramain/review/problem/controller/ProblemController.java
@@ -2,7 +2,10 @@ package def.agoramain.review.problem.controller;
 
 import def.agoramain.review.problem.service.ProblemService;
 import def.agoramain.review.problem.dto.request.ProblemRequestDto;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,15 +13,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Tag(name = "Problem", description = "회고 속 문제에 대한 API")
 @RestController()
 @RequestMapping("/problem")
 @RequiredArgsConstructor
 public class ProblemController {
     private final ProblemService problemService;
 
-    @ApiOperation(value="문제 저장", notes = "회고에 대한 문제를 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "문제 생성 성공"),
+            @ApiResponse(responseCode = "500", description = "문제 생성 실패")
+    })
+    @Operation(summary = "문제 생성", description = "회고에 대한 문제를 생성합니다.")
     @PostMapping()
-    public void saveProblem(@Valid @RequestBody ProblemRequestDto problemRequestDto)throws Exception{
+    public void saveProblem(@Valid @RequestBody ProblemRequestDto problemRequestDto){
         this.problemService.saveProblem(problemRequestDto);
     }
 }
