b7e05bf9090ebbf4ad5c18dea91ee5bf46425114
BaeKwangho <jason@monymony.co>
2024-03-30 20:05:19 +0900

    docs: 회고 API 설명 및 docker 이미지 관리 방법 설명 추가

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # agora-main
+
+## CI/CD
+### Docker Image Management
+(임시) 특정 dev 커밋에 버전을 태깅하여, 동일한 태그로 docker hub에 push
+- 작업 완료 후 develop branch 에 tag 를 달아 커밋 푸시
+  - tag 는 `vx.x.n` 이며, develop branch 에서는 n 을 수정
+  - 숫자는 이전 버전보다 높기만 하면 괜찮을 듯?
+  ```
+  git tag v0.1.0 // 특정 커밋에 태그 생성
+  git push origin tag v0.1.0 // 태그를 remote 에 push
+  ```
+- docker image build 및 push
+  
+  ```
+  ./gradlew build
+  docker build . --tag teamdef/agora-back-main:0.1.0
+  docker push teamdef/agora-back-main:0.1.0
+  ```
+
+  - latest 로 추가하려면, 위 커맨드 이후 아래 추가 실행 
+    ```
+    docker push teamdef/agora-back-main:latest
+    ```
--- src/main/java/def/agoramain/retro/RetroController.java
+++ src/main/java/def/agoramain/retro/RetroController.java
@@ -3,6 +3,11 @@ package def.agoramain.retro;
 import def.agoramain.retro.dto.ReqRetroDto;
 import def.agoramain.retro.dto.RetroDetailDto;
 import def.agoramain.retro.dto.RetroDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Retro", description = "회고에 대한 API")
 @RestController
 @RequestMapping("/retro")
 @CrossOrigin(origins = "*")
@@ -18,6 +24,11 @@ public class RetroController {
 
     private final RetroService retroService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회고 목록 조회 성공"),
+            @ApiResponse(responseCode = "500", description = "회고 목록 조회 실패")
+    })
+    @Operation(summary = "회고 목록 조회", description = "프로젝트의 회고 목록을 조회합니다.")
     @GetMapping
     public ResponseEntity<List<RetroDto>> getRetros(
             @RequestParam int pageNo,
@@ -26,14 +37,22 @@ public class RetroController {
 
         return ResponseEntity.ok(retroService.findRetros(projectId, PageRequest.of(pageNo - 1, listSize)));
     }
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회고 상세 조회 성공"),
+            @ApiResponse(responseCode = "500", description = "회고 상세 조회 실패", content = @Content)
+    })
+    @Operation(summary = "회고 상세 조회", description = "지속할 점, 문제와 연관된 시도를 포함한 상세 정보를 조회합니다.")
     @GetMapping("/{retroId}")
     public ResponseEntity<RetroDetailDto> getRetroDetail(
             @PathVariable Long retroId) throws Exception{
 
         return ResponseEntity.ok(retroService.findRetroDetail(retroId));
     }
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회고 생성 성공"),
+            @ApiResponse(responseCode = "500", description = "회고 생성 실패", content = @Content)
+    })
+    @Operation(summary = "회고 생성", description = "프로젝트의 회고를 생성합니다.")
     @PostMapping
     public void makeRetro(
             @RequestBody ReqRetroDto reqRetro) {
@@ -41,6 +60,11 @@ public class RetroController {
         retroService.makeRetro(reqRetro);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회고 삭제 성공"),
+            @ApiResponse(responseCode = "500", description = "회고 삭제 실패", content = @Content)
+    })
+    @Operation(summary = "회고 삭제", description = "회고를 삭제합니다.")
     @DeleteMapping("/{retroId}")
     public void deleteRetro(
             @PathVariable("retroId") Long retroId
@@ -49,6 +73,11 @@ public class RetroController {
         retroService.deleteRetro(retroId);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회고 참여자 삭제 성공"),
+            @ApiResponse(responseCode = "500", description = "회고 참여자 삭제 실패", content = @Content)
+    })
+    @Operation(summary = "회고 참여자 삭제", description = "요청된 회고에 참여한 유저들을 삭제합니다.")
     @DeleteMapping("/{retroId}/members")
     public void deleteRetro(
             @PathVariable("retroId") Long retroId,
